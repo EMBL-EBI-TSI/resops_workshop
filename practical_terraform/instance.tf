10d68b6a11a2eac17d96c647ff165c5a5df0a4ac
erikvdbergh <erikvdbergh@gmail.com>
2017-05-24 15:17:29 +0000

    add secgroup

--- practical_terraform/instance.tf
+++ practical_terraform/instance.tf
@@ -18,13 +18,25 @@ resource "openstack_networking_subnet_v2" "demo_subnet" {
   ip_version = 4
 }
 
+# Create a security group
+resource "openstack_compute_secgroup_v2" "demo_secgroup" {
+  name = "${var.name}_secgroup"
+
+  rule {
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+}
+
 # Create a web server
 resource "openstack_compute_instance_v2" "basic" {
   name            = "${var.name}_machine"
   image_id        = "7b5fea5e-7a75-46de-a1c4-5e7fae18eb85"
   flavor_name     = "s1.tiny"
   key_pair        = "${openstack_compute_keypair_v2.demo_keypair.name}"
-  security_groups = ["base_vms_basic_sec"]
+  security_groups = ["${openstack_compute_secgroup_v2.demo_secgroup.name"]
 
   network {
     name = "${openstack_networking_network_v2.demo_network.name}"
