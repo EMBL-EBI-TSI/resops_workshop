21134c012386b7df23af33de5dcbe6f6516e75d4
dvianello <dario.vianello87@gmail.com>
2017-06-02 13:12:29 +0100

    Update destroy.sh to match deploy.sh

--- practical_cluster/destroy.sh
+++ practical_cluster/destroy.sh
@@ -4,7 +4,7 @@ set -e
 # The script assumes that env vars for authentication with OpenStack are already present.
 
 ## Customise this ##
-export TF_VAR_DEPLOYMENT_KEY_PATH="/path/to/your/key"
+export TF_VAR_DEPLOYMENT_KEY_PATH="path_to_your_private_key"
 
 # Destroy everything
 cd terraform || exit
